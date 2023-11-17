ae083bf7d7a75130a25763dc0155f706d73bf5c8
Vinicius Reis <vinicius.reis@msn.com>
2023-11-16 22:28:16 -0300

    implements fechador Sessoes abertas

--- src/main/java/academy/wakanda/wakacop/sessaovotacao/infra/SessaoVotacaoInfraRepository.java
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/infra/SessaoVotacaoInfraRepository.java
@@ -2,10 +2,12 @@ package academy.wakanda.wakacop.sessaovotacao.infra;
 
 import academy.wakanda.wakacop.sessaovotacao.application.service.SessaoVotacaoRepository;
 import academy.wakanda.wakacop.sessaovotacao.domain.SessaoVotacao;
+import academy.wakanda.wakacop.sessaovotacao.domain.StatusSessaoVotacao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -30,5 +32,13 @@ public class SessaoVotacaoInfraRepository implements SessaoVotacaoRepository {
                         .orElseThrow(() -> new RuntimeException("Sessão não encontrada!"));
         log.info("[finish] SessaoVotacaoInfraRepository - buscaPorId");
         return sessao;
+    }
+
+    @Override
+    public List<SessaoVotacao> buscaAbertas() {
+        log.debug("[start] SessaoVotacaoInfraRepository - buscaAbertas");
+        List<SessaoVotacao> sessoes = sessaoVotacaoSpringDataJPARepository.findByStatus(StatusSessaoVotacao.ABERTA);
+        log.debug("[finish] SessaoVotacaoInfraRepository - buscaAbertas");
+        return sessoes;
     }
 }
